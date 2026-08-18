5683b03e38035a7d9a90a0a3e0f9bede71e64d0b
gabriel <gabrielpogamer@gmail.com>
2026-08-18 17:11:54 -0300

    mudanças muito pequenas

--- front-end/src/app/features/login-instituicao/login-instituicao.ts
+++ front-end/src/app/features/login-instituicao/login-instituicao.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from "@angular/router";
+
 
 @Component({
   selector: 'app-login-instituicao',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login-instituicao.html',
   styleUrl: './login-instituicao.css',
 })
-export class LoginInstituicao {}
+export class LoginInstituicao {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', Validators.required],
+      lembrar: [false]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const dados = this.loginForm.value;
+      // espaço para serviço HTTP de POST aqui enviando os dados
+    }
+  }
+}
